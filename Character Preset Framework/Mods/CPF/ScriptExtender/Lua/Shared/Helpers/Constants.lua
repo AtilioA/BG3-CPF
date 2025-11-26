eeaa5e5b13f7bf476f9a7bfb121be3703c414536
Atílio <27153776+AtilioA@users.noreply.github.com>
2025-11-26 13:10:17 -0300

    refactor: use mod version as preset schema

--- Character Preset Framework/Mods/CPF/ScriptExtender/Lua/Shared/Helpers/Constants.lua
+++ Character Preset Framework/Mods/CPF/ScriptExtender/Lua/Shared/Helpers/Constants.lua
@@ -1,5 +1,5 @@
 Constants = {}
 
-Constants.PRESET_SCHEMA = '1'
+Constants.PRESET_SCHEMA = table.concat(Ext.Mod.GetMod("4fa17abe-993c-4e7e-ab2a-e7370b166ac9").Info.ModVersion, ".")
 
 return Constants
