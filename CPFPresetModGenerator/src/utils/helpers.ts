883322c644581ba21d13af17e3366bdb4fa88270
Atílio <27153776+AtilioA@users.noreply.github.com>
2025-11-30 15:41:43 -0300

    fix(web): escape/encode characters for xml files

--- CPFPresetModGenerator/src/utils/helpers.ts
+++ CPFPresetModGenerator/src/utils/helpers.ts
@@ -24,6 +24,19 @@ export const sanitizeFolderName = (name: string): string => {
 };
 
 /**
+ * Escapes special XML characters to prevent XML parsing errors.
+ * Converts: & < > " ' to their XML entity equivalents.
+ */
+export const escapeXml = (text: string): string => {
+    return text
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&apos;');
+};
+
+/**
  * Generates the final folder name with CPF prefix and UUID suffix.
  * Truncates the user-provided name to 36 characters to avoid path length issues.
  */
@@ -67,7 +80,7 @@ export const generateDependenciesXml = (dependencies: ModDependency[]): string =
                         <node id="ModuleShortDesc">
                             <attribute id="Folder" type="LSString" value=""/>
                             <attribute id="MD5" type="LSString" value=""/>
-                            <attribute id="Name" type="LSString" value="${dep.modName}"/>
+                            <attribute id="Name" type="LSString" value="${escapeXml(dep.modName)}"/>
                             <attribute id="PublishHandle" type="uint64" value="0"/>
                             <attribute id="UUID" type="guid" value="${dep.modUUID}"/>
                             <attribute id="Version64" type="int64" value="36028797018963968"/>
@@ -79,10 +92,10 @@ export const generateDependenciesXml = (dependencies: ModDependency[]): string =
  */
 export const generateMetaLsx = (config: ModConfig): string => {
     let xml = META_LSX_TEMPLATE;
-    xml = xml.replace('{{AUTHOR}}', config.author);
-    xml = xml.replace('{{DESCRIPTION}}', config.description);
-    xml = xml.replace('{{FOLDER}}', getGeneratedFolderName(config.folderName, config.uuid));
-    xml = xml.replace('{{NAME}}', config.modName);
+    xml = xml.replace('{{AUTHOR}}', escapeXml(config.author));
+    xml = xml.replace('{{DESCRIPTION}}', escapeXml(config.description));
+    xml = xml.replace('{{FOLDER}}', escapeXml(getGeneratedFolderName(config.folderName, config.uuid)));
+    xml = xml.replace('{{NAME}}', escapeXml(config.modName));
     xml = xml.replace('{{UUID}}', config.uuid);
 
     // Add additional mod dependencies after CPF dependency only if includeDependencies is true
