44af1b7c5a6bb01f9499fe9bfa72746ba3ef8e6d
Atílio <27153776+AtilioA@users.noreply.github.com>
2025-11-30 13:36:08 -0300

    feat: add Sentry for error observability

---
+++ CPFPresetModGenerator/src/instrumentation-client.ts
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://51d8b482c6cb9107b355b9f047adfae0@o4510454219014144.ingest.de.sentry.io/4510454272819280",
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
