baad1776175a971bc19b134755b9f3ada92f2b90
Atílio <27153776+AtilioA@users.noreply.github.com>
2025-12-02 10:41:26 -0300

    refactor: modularize `CreateMode`

--- Character Preset Framework/Mods/CPF/ScriptExtender/Lua/Client/UI/Modes/CreateMode.lua
+++ Character Preset Framework/Mods/CPF/ScriptExtender/Lua/Client/UI/Modes/CreateMode.lua
@@ -1,5 +1,6 @@
 local State = Ext.Require("Client/UI/State.lua")
 local RenderHelper = Ext.Require("Client/UI/RenderHelper.lua")
+local CreateModeHelpers = Ext.Require("Client/UI/Modes/CreateModeHelpers.lua")
 
 local CreateMode = {}
 
@@ -12,73 +13,19 @@ function CreateMode:Render(parent)
         -- Create reactive group for input fields that updates when NewPresetData changes
         RenderHelper.CreateReactiveGroup(g, "PresetInputFields", State.NewPresetData,
             function(inputGroup, presetData)
-                inputGroup:AddText(Loca.Get(Loca.Keys.UI_LABEL_NAME))
-                local inputName = inputGroup:AddInputText("")
-                inputName.SameLine = true
-                inputName.Text = presetData.Name
-                local data = State.NewPresetData:GetValue()
-                inputName.OnChange = function()
-                    data.Name = inputName.Text
-                end
-                inputName.OnDeactivate = function()
-                    State.NewPresetData:OnNext(data)
-                end
-
-                inputGroup:AddText(Loca.Get(Loca.Keys.UI_LABEL_AUTHOR))
-                local inputAuthor = inputGroup:AddInputText("")
-                inputAuthor.SameLine = true
-                inputAuthor.Text = presetData.Author
-                inputAuthor.OnChange = function()
-                    data.Author = inputAuthor.Text
-                end
-
-                inputAuthor.OnDeactivate = function()
-                    State.NewPresetData:OnNext(data)
-                end
-
-                inputGroup:AddText(Loca.Get(Loca.Keys.UI_LABEL_VERSION))
-                local inputVer = inputGroup:AddInputText("")
-                inputVer.SameLine = true
-                inputVer.Text = presetData.Version
-                inputVer.OnChange = function()
-                    data.Version = inputVer.Text
-                end
-
-                inputVer.OnDeactivate = function()
-                    State.NewPresetData:OnNext(data)
-                end
+                CreateModeHelpers.RenderInputFields(inputGroup, presetData)
             end)
 
-        local btnSave = g:AddButton(Loca.Get(Loca.Keys.UI_BUTTON_SAVE))
-        btnSave:SetColor("Button", UIColors.COLOR_GREEN)
-        btnSave.OnClick = function()
-            State:SaveNewPreset()
-        end
-
-        btnSave.SameLine = false
-
-        local btnCancel = g:AddButton(Loca.Get(Loca.Keys.UI_BUTTON_CANCEL))
-        btnCancel.OnClick = function()
-            State:SetMode("VIEW")
-        end
-
-        btnCancel.SameLine = true
+        CreateModeHelpers.RenderActionButtons(g)
 
         g:AddSeparator()
         local detailsCH = g:AddCollapsingHeader(Loca.Get(Loca.Keys.UI_HEADER_CAPTURED_ATTRIBUTES))
         detailsCH.DefaultOpen = false
+
         -- Create reactive preview that updates when CapturedData changes
         RenderHelper.CreateReactiveGroup(detailsCH, "CapturedDataPreview", State.CapturedData,
             function(previewGroup, capturedData)
-                local previewChild = previewGroup:AddChildWindow("CreatePreview")
-                previewChild.Size = { 0, 500 }
-
-                if capturedData then
-                    -- Show captured data
-                    previewChild:AddText(Ext.Json.Stringify(capturedData))
-                else
-                    previewChild:AddText(Loca.Get(Loca.Keys.UI_MSG_NO_DATA_CAPTURED))
-                end
+                CreateModeHelpers.RenderCapturedDataPreview(previewGroup, capturedData)
             end)
     end)
 end
---
+++ Character Preset Framework/Mods/CPF/ScriptExtender/Lua/Client/UI/Modes/CreateModeHelpers.lua
@@ -0,0 +1,58 @@
+local State = Ext.Require("Client/UI/State.lua")
+
+local CreateModeHelpers = {}
+
+function CreateModeHelpers.CreateInputField(group, labelKey, fieldName, presetData)
+    group:AddText(Loca.Get(labelKey))
+    local input = group:AddInputText("")
+    input.SameLine = true
+    input.Text = presetData[fieldName]
+
+    local data = State.NewPresetData:GetValue()
+    input.OnChange = function()
+        data[fieldName] = input.Text
+    end
+
+    input.OnDeactivate = function()
+        State.NewPresetData:OnNext(data)
+    end
+
+    return input
+end
+
+function CreateModeHelpers.RenderInputFields(inputGroup, presetData)
+    CreateModeHelpers.CreateInputField(inputGroup, Loca.Keys.UI_LABEL_NAME, "Name", presetData)
+    CreateModeHelpers.CreateInputField(inputGroup, Loca.Keys.UI_LABEL_AUTHOR, "Author", presetData)
+    CreateModeHelpers.CreateInputField(inputGroup, Loca.Keys.UI_LABEL_VERSION, "Version", presetData)
+end
+
+function CreateModeHelpers.RenderActionButtons(group)
+    local btnSave = group:AddButton(Loca.Get(Loca.Keys.UI_BUTTON_SAVE))
+    btnSave:SetColor("Button", UIColors.COLOR_GREEN)
+    btnSave.OnClick = function()
+        State:SaveNewPreset()
+    end
+    btnSave.SameLine = false
+
+    local btnCancel = group:AddButton(Loca.Get(Loca.Keys.UI_BUTTON_CANCEL))
+    btnCancel.OnClick = function()
+        State:SetMode("VIEW")
+    end
+    btnCancel.SameLine = true
+
+    return btnSave, btnCancel
+end
+
+function CreateModeHelpers.RenderCapturedDataPreview(previewGroup, capturedData)
+    local previewChild = previewGroup:AddChildWindow("CreatePreview")
+    previewChild.Size = { 0, 500 }
+
+    if capturedData then
+        -- Show captured data
+        previewChild:AddText(Ext.Json.Stringify(capturedData))
+    else
+        previewChild:AddText(Loca.Get(Loca.Keys.UI_MSG_NO_DATA_CAPTURED))
+    end
+end
+
+return CreateModeHelpers
