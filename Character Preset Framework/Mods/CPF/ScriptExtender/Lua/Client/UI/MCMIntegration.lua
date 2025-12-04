a21eae7d80c0f8ec3c7ca062b191858a38bf6efd
Atílio <27153776+AtilioA@users.noreply.github.com>
2025-12-04 18:59:41 -0300

    feat: make it compatible with MCM 1.37.1

--- Character Preset Framework/Mods/CPF/ScriptExtender/Lua/Client/UI/MCMIntegration.lua
+++ Character Preset Framework/Mods/CPF/ScriptExtender/Lua/Client/UI/MCMIntegration.lua
@@ -1,7 +1,64 @@
 MCMIntegration = {}
 
 local Window = Ext.Require("Client/UI/Window.lua")
+local State = Ext.Require("Client/UI/State.lua")
+
 function MCMIntegration.Initialize()
+    -- Create CPF window and render presets
+    MCM.InsertModMenuTab("Preset manager", function(window)
+        Window:RenderCPF(window)
+    end, ModuleUUID)
+
+    MCM.EventButton.RegisterCallback("unhide_presets", function()
+        -- REFACTOR: make this less scattered and with mixed responsibilities
+        -- Ideally, some things would be done via reactivex
+        local unhid = PresetIndex:UnhideAll()
+        PresetRegistry:Clear()
+        PresetDiscovery:LoadPresets()
+        State:RefreshPresets()
+        if unhid then
+            MCM.EventButton.ShowFeedback(
+                "unhide_presets",
+                Loca.Get(Loca.Keys.MCM_MSG_UNHIDDEN_SUCCESS),
+                "success"
+            )
+        else
+            MCM.EventButton.ShowFeedback(
+                "unhide_presets",
+                Loca.Get(Loca.Keys.MCM_MSG_UNHIDDEN_FAILURE),
+                "error"
+            )
+        end
+    end)
+
+    MCM.EventButton.RegisterCallback("reset_presets_index", function()
+        local cleared = PresetIndex:Clear()
+        local loaded = PresetDiscovery:LoadPresets()
+        if cleared and loaded >= 0 then
+            MCM.EventButton.ShowFeedback(
+                "reset_presets_index",
+                Loca.Format(Loca.Keys.MCM_MSG_RESET_SUCCESS, loaded),
+                "success"
+            )
+        else
+            MCM.EventButton.ShowFeedback(
+                "reset_presets_index",
+                Loca.Get(Loca.Keys.MCM_MSG_RESET_FAILURE),
+                "error"
+            )
+        end
+    end)
+
+    MCM.Keybinding.SetCallback('open_cpf', function()
+        MCM.OpenModPage(
+            Loca.Get(Loca.Keys.MCM_TAB_PRESET_MANAGER),
+            ModuleUUID,
+            true
+        )
+    end)
+end
+
+function MCMIntegration.Initialize_MCM_1_38()
     -- Create CPF window and render presets
     MCM.InsertModMenuTab({
         tabCallback = function(window) Window:RenderCPF(window) end,
